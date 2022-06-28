025252729caeb620b9372375695ba6cfeaee43ae
armaBurton <armanarma@armaburton.com>
2022-06-28 14:33:17 -0700

    first row of contacts is entered and styles, styles should apply to new rows without conflict

--- src/components/LeftSection/LeftSection.jsx
+++ src/components/LeftSection/LeftSection.jsx
@@ -5,6 +5,13 @@ export default function leftSection() {
     <section className={styles.leftSection}>
       <h1 className={styles.name}>Arma Burton</h1>
       <h3 className={styles.title}>software engineer</h3>
+      <div className={styles.contactLinkContainer}>
+        <div className={styles.contactLinkRow}>
+          <p className={styles.contactMethod}>github</p>
+          <div className={styles.leaderDots}></div>
+          <p className={styles.contactLink}>/armaBurton</p>
+        </div>
+      </div>
     </section>
   );
 }
