4b34f9951088960d9e23c32d9fa4340cb3495251
armaBurton <armanarma@armaburton.com>
2022-06-28 10:41:11 -0700

    colors ahve been added to index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import styles from './App.css';
+
 export default function App() {
   return (
     <>
       <body>
-        <section>adflkawejflkawejfawe</section>
+        <section
+          className={styles.portfolioHolder}
+          style={{ backgroundColor: 'yellow' }}
+        >
+          <section className={styles.leftSection}>left</section>
+          <section className={styles.rightSection}>right</section>
+        </section>
       </body>
     </>
   );
