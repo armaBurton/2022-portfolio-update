bab5a2b3d216fd6bf58645e5fd2085a3961b8aa0
armaBurton <armanarma@armaburton.com>
2022-06-29 13:23:04 -0700

    education has been added

--- src/components/LeftSection/Education/Education.jsx
+++ src/components/LeftSection/Education/Education.jsx
@@ -1,7 +1,22 @@
+import styles from './Education.css';
+
 export default function Education() {
   return (
-    <>
-      <h3>alchemy code lab</h3>
-    </>
+    <div className={styles.eduContainer}>
+      <h3 className={styles.name}>alchemy code lab</h3>
+      <p className={styles.date}>November 2021-June 2022</p>
+      <ul className={styles.eduUl}>
+        <li className={styles.eduList}>
+          &nbsp;1000 Hours of intensive remote Software Engineering Training
+        </li>
+        <li className={styles.eduList}>
+          &nbsp;Multiple week-long remote project sprints focusing on group
+          collaboration, TDD, planning, and app deployment
+        </li>
+      </ul>
+      <h3 className={styles.name}>portland state university</h3>
+      <p className={styles.date}>September 2012-June 2016</p>
+      <p className={styles.degree}>BS Graphic Design</p>
+    </div>
   );
 }
